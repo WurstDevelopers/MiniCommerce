68b74e3c140b6ad5fcc3fe1ec917c38ec7bdf491
malenso <mfensor@gmail.com>
2018-01-31 12:55:53 -0600

    Thomas was great and came to work to help me write my first test about product manager delete function. I will try and debug why tests don't run on my machine, I will also write two tests (keep one failing.

---
+++ MiniCommerce.Domain.Tests/ProductManagementTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniCommerce.Domain.Security;
+
+namespace MiniCommerce.Domain.Tests
+{
+
+    [TestClass]
+    public class ProductManagementTests
+    {
+        [TestMethod]
+        public void BadTestName1()
+        {
+            var authorizer = new MockAuthorizer();
+            var productManager = new ProductManagement(authorizer);
+            var product = new Product();
+
+            productManager.Delete(product);
+
+            authorizer.PermissionRequiredInput.Should().Be(Permission.Delete);
+        }
+
+        //read
+        
+        //upsert maybe several
+
+        //try to figure out next step for delete
+    }
+
+
+    public class MockAuthorizer : IAuthorizer
+    {
+        public Permission PermissionRequiredInput { get; set; }
+
+        public bool HasAuthorization(Permission permissionRequired)
+        {
+            PermissionRequiredInput = permissionRequired;
+            return false;
+        }
+    }
+}
---
+++ MiniCommerce.Domain/ProductManagement.cs
@@ -0,0 +1,20 @@
+﻿
+using MiniCommerce.Domain.Security;
+
+namespace MiniCommerce.Domain
+{
+    public class ProductManagement
+    {
+        private readonly IAuthorizer authorizer;
+
+        public ProductManagement(IAuthorizer authorizer)
+        {
+            this.authorizer = authorizer;
+        }
+
+        public void Delete(Product product)
+        {
+            authorizer.HasAuthorization(Permission.Delete);
+        }
+    }
+}
