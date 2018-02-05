5b0ada0f83d697402d24f06d767759c3fd46e5e3
malenso <mfensor@gmail.com>
2018-02-05 17:30:30 -0600

    Updated files to all use .dotnet 4.6.1

---
+++ ConsoleApp1/ProductManagementTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniCommerce.Domain.Security;
+
+namespace MiniCommerce.Domain.Tests
+{
+
+    [TestClass]
+    public class ProductManagementTests
+    {
+        [TestMethod]
+        public void WhenProductIsDeletedPermissionShouldBeDelete()
+        {
+            var authorizer = new MockAuthorizer();
+            var productManager = new ProductManagement(authorizer);
+            var product = new Product();
+
+            productManager.Delete(product);
+
+            authorizer.PermissionRequiredInput.Should().Be(Permission.ProductDelete);
+        }
+
+        //try to figure out next step for delete
+        
+        [TestMethod]
+        public void WhenProductIsUpdatedPermissionShouldBeWrite()
+        {
+            var authorizer = new MockAuthorizer();
+            var productManager = new ProductManagement(authorizer);
+            var product = new Product();
+
+            productManager.Update(product);
+
+            authorizer.PermissionRequiredInput.Should().Be(Permission.Write);
+        }
+
+        [TestMethod]
+        public void WhenRetrievingProductPermissionShouldBeRead()
+        {
+            //It is going to be difficult to test the read permission. 
+            //Enums default to the first option of the enum. So, if make sure that Permission.Read is passed in 
+            //you need to overwrite the default to something else in order to get a failing test.
+            var authorizer = new MockAuthorizer();
+            var productManager = new ProductManagement(authorizer);
+            var product = new Product();
+
+            productManager.Read(product);
+            authorizer.PermissionRequiredInput.Should().Be(Permission.Read);
+        }
+    }
+
+
+    public class MockAuthorizer : IAuthorizer
+    {
+        public Permission PermissionRequiredInput { get; set; }
+
+        public bool HasAuthorization(Permission permissionRequired)
+        {
+            PermissionRequiredInput = permissionRequired;
+            return false;
+        }
+    }
+}
---
+++ MiniCommerce.Infrastructure.Test/UserManagementTests.cs
@@ -0,0 +1,107 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniCommerce.Infrastructure.Interfaces;
+using RandomTestValues;
+
+namespace MiniCommerce.Infrastructure.Tests
+{
+    [TestClass]
+    public class UserManagementTests
+    {
+        private MockHasher hasher;
+        private MockUserRepository userRepository;
+        private UserManagement userManager;
+
+        [TestInitialize]
+        public void RunBeforeBeforeTestMethods()
+        {
+            hasher = new MockHasher();
+            userRepository = new MockUserRepository();
+            userManager = new UserManagement(hasher, userRepository);
+        }
+
+        [TestMethod]
+        public void UserManagementTestsCreateUserShouldCallThePasswordHasherWithTheCorrectParameters()
+        {
+            var password = "BlueIsMyFavoriteCat(ThisIsTrue)";
+            var userName = "Thomas";
+
+            userManager.CreateUser(userName, password);
+
+            hasher.CreatePasswordHashInput.Should().Be(password);
+        }
+
+        [TestMethod]
+        public void UserManagementTestsCreateUserShouldCallAddUserWithTheCorrectUserName()
+        {
+            var username = RandomValue.String();
+            var password = RandomValue.String();
+
+            userManager.CreateUser(username, password);
+
+            userRepository.UserToAddInput.UserName.Should().Be(username);
+        }
+
+        [TestMethod]
+        public void UserManagementTestsCreateUserShouldPassPasswordHashFromIHasherToTheDataRepoOnTheUserObject()
+        {
+            var username = RandomValue.String();
+            var password = RandomValue.String();
+
+            var hashToReturn = RandomValue.Object<PasswordHash>();
+
+            hasher.CreatePasswordHashReturnObject = hashToReturn;
+
+            userManager.CreateUser(username, password);
+
+            //the BeSameAs assert means that it is the EXACT same object as hashToReturn.
+            userRepository.UserToAddInput.PasswordHash.Should().BeSameAs(hashToReturn);
+            userRepository.UserToAddInput.PasswordHash.Hash.Should().Be(hashToReturn.Hash);
+        }
+
+        [TestMethod]
+        public void UserManagementTestsCreateUserShouldCallTheAccountExistsWithCorrectParameters()
+        {
+            var username = RandomValue.String();
+            var password = RandomValue.String();
+
+            userManager.CreateUser(username, password);
+
+            userRepository.AccountExistsInput.Should().Be(username);
+        }
+
+        // create password hash should not be called
+
+        // exception should be thrown
+    }
+
+    public class MockUserRepository : IUserRepsository
+    {
+        public User UserToAddInput { get; private set; }
+        public string AccountExistsInput { get; private set; }
+        public bool AccountExistsReturnBool { private get; set; }
+
+        public bool AccountExists(string userName)
+        {
+            AccountExistsInput = userName;
+            return AccountExistsReturnBool;
+        }
+
+        public void AddUser(User userToAdd)
+        {
+            UserToAddInput = userToAdd;
+        }
+    }
+
+    public class MockHasher : IHasher
+    {
+        public string CreatePasswordHashInput { get; private set; }
+        public PasswordHash CreatePasswordHashReturnObject { private get; set; }
+
+        public PasswordHash CreatePasswordHash(string password)
+        {
+            CreatePasswordHashInput = password;
+            return CreatePasswordHashReturnObject;
+        }
+    }
+}
